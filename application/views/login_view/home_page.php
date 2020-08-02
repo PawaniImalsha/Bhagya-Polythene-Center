0ead1847fcba7523bf1ad4794dcc9a47be429bd8
Pawani Imalsha <imalshapawani95@gmail.com>
2020-08-02 17:09:00 +0530

    add the project

---
+++ application/views/login_view/home_page.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Polithin page 1</title>
+
+	
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/home_page.css')?> " >
+	
+	
+
+	<STYLE>A {text-decoration: none;} </STYLE><!--Remove the Underline from All Hyperlinks-->
+
+	<div class="top-bar">
+
+		<center>
+			
+			<div class="top-bar-links">
+
+			<ul>
+				
+				<li><a href="<?php echo site_url('login_control/sign_in'); ?>">SIGN IN</a></li>
+				<li><a href="<?php echo site_url('login_control/register'); ?>">REGISTER</a></li>
+				<li><a href="<?php echo site_url('login_control/products'); ?>">PRODUCTS</a></li>
+				<li><a href="<?php echo site_url('login_control/location'); ?>">SHOPS LOCATIONS</a></li>
+
+				</ul>
+		
+		</div><!--top-bar-links-->
+
+	</center>
+	
+</div><!--top-bar-->
+<!--<img src="?php echo base_url('assets/images/background.jpg');?>" alt="" style="width : 1900px;"-->
+
+</head>
+<body>
+
+<div class="topic">
+<center>
+	
+	<h1>Welcome to Bagya Polythene Center</h1>
+</center>
+</div>
+
+<div class="about1">
+<h3>What Are The Different Types of Polyethylene?</h3>
+<p>
+Polyethylene is commonly categorized into one of several major compounds of which the
+most common include LDPE, LLDPE, HDPE, and Ultrahigh Molecular Weight Polypropylene. 
+Other variants include Medium Density Polyethylene (MDPE), 
+Ultra-low-molecular-weight polyethylene (ULMWPE or PE-WAX),
+High-molecular-weight polyethylene (HMWPE), 
+High-density cross-linked polyethylene (HDXLPE), 
+Cross-linked polyethylene (PEX or XLPE), Very-low-density polyethylene (VLDPE), 
+and Chlorinated polyethylene (CPE).
+</p>
+</div>
+
+<div class="video">
+<h2>Environmentally friendly process of plastics recycling</h2>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/x9rXCPlRF78?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<!--iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1"-->
+</!--iframe>
+</div>
+
+
+
+
+</body>
+</html>
---
+++ application/views/login_view/shop_item_page.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+<meta charset="utf-8">
+<meta name="viewport" content="width = device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<!--link rel="stylesheet" type="text/css" href="css/common.css"-->
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/common.css')?> " >
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/home_page.css')?> " >
+
+	<!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/sign_page.css')?> " >
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/Footer-with-social-icons.css')?> " > -->
+
+
+	<STYLE>A {text-decoration: none;} </STYLE><!--Remove the Underline from All Hyperlinks-->
+
+	<center>
+		
+		<div class="top-bar">
+
+			
+			<div class="top-bar-links">
+
+		<ul>
+			
+            <li><a href="<?php echo site_url('login_control/home'); ?>">HOME</a></li>
+            <li><a href="<?php echo site_url('login_control/sign_in'); ?>">SIGN IN</a></li>
+			<li><a href="<?php echo site_url('login_control/register'); ?>">REGISTER</a></li>
+			<li><a href="<?php echo site_url('login_control/location'); ?>">SHOPS LOCATIONS</a></li>
+
+		</ul>
+		
+	</div><!--top-bar-links-->
+		
+	</div><!--top-bar-->
+
+	</center>
+	
+<!--top-bar-->
+	<!--<img src="?php echo base_url('assets/images/background.jpg');?>" alt="" style="width : 1900px;"-->
+
+
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width = device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <!--link rel="stylesheet" type="text/css" href="css/common.css"-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/common.css')?> " >
+</head>
+<body>
+        <div id="section1" class="container-fluid">
+                <!--start section1-->
+                <div class="container">
+                    
+                        <!-- <div class="image">
+                                <!--start image-->
+                                <!-- <img src="image/logo.png">
+                                <!--end of image-->
+                        <!-- </div> --> 
+                        <div style="background-color: #5D6D7E ; margin-left: 10px;padding-left: 20px;padding-top: 20px;margin-bottom: 20px;margin-top: 20px;">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src= "<?php echo base_url('assets/images/shop_items/img1.jpg')?>">
+                        <h3>Jumbo Bag</h3>
+
+                               <p> We are among the prominent manufactures, suppliers of an substantial range of premium quality Jumbo bag in the industry. Our large production capacity, we are able to offer jumbo bags in bulk quantities 
+                             
+                                Features
+                             *Light in weight
+                             *Easy to carry
+                             *Water resistant
+                             </p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="<?php echo base_url('assets/images/shop_items/img2.jpg')?>">
+                        <h3>Builder Films</h3>
+                        <p>Uses: 
+                                Temporary weather protection during building.
+                                Moisture barrier under concrete slabs
+                                Protects plants from birds 
+                                Drop sheet</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="<?php echo base_url('assets/images/shop_items/img3.jpg')?>">
+                        <h3>Garbage Bags</h3>
+                        <p>These garbage bags can be produced in different
+                                sizes,   materials and colours for your specific needs.
+                               ** Garbage bags are made out of 100% recycled plastic</p>
+                    </div>
+                    
+                    
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="<?php echo base_url('assets/images/shop_items/img4.jpg')?>">
+                        <h3>Hardware Films</h3>
+                        <p>All our cargo covers / films are made out of 100% recycled plastic.</p>
+                    </div>
+                    
+                    <div class="col-md-4">
+                        <img src="<?php echo base_url('assets/images/shop_items/img6.jpg')?>">
+                        <h3>Laminated Roll Form & Bags</h3>
+                        <p>
+                            - Films with specialized printing - up to 16 Colors
+                             - Laminated bags for all industry</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="<?php echo base_url('assets/images/shop_items/img7.jpg')?>">
+                        <h3>Road Mark & Warning Tape</h3>
+                        <p>
+                         Manufacturer warning tapes, underground warning tapes, barricade tapes, warning tapes for electrical line, Construction safety tapes, Cable safety tapes, Printed warning tapes and message warning tapes.</p>
+
+                    </div>
+                   
+                    
+                </div>
+                <div class="row">
+                     <div class="col-md-4">
+                        <img src="<?php echo base_url('assets/images/shop_items/img8.jpg')?>">
+                        <h3>Clappers</h3>
+                        <p>Makes sound like clapping the metal, It is a new type for Promotion, Cheering sticks are available in different colors and Printing Options. Mainly used in all kinds of sports games, vocal Performances, Parties</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="<?php echo base_url('assets/images/shop_items/img5.jpg')?>">
+                        <h3>Polythene Roll Form</h3>
+
+                               <p> 
+
+            Product Specifications:
+            Material Type: HDPE/LDPE
+            Maximum Width Size: LDPE 50”
+            Length: Custom Made
+            Size: Custom Made
+            Thickness: Custom Made
+                             </p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="<?php echo base_url('assets/images/shop_items/img9.jpg')?>">
+                        <h3>Disposable Plastic Aprons</h3>
+                        <p>This Product Is Disposable Hygiene Products, Can Be Used In Hotels, Shopping Malls, Hotels, Hospitals, Family Life And Other Places.</p>
+                    </div>
+
+                </div>
+            </div>
+                </div>
+            </div>
+
+</body>
+</html>
